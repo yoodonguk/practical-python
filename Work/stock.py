cbbd35c2045dcee69b3981da382ce630830aecf7
yoodonguk <34823253+yoodonguk@users.noreply.github.com>
2020-10-06 01:11:41 +0900

    Study
    2020.10.06 01:10

--- Work/stock.py
+++ Work/stock.py
@@ -14,6 +14,8 @@ class Stock():
     def cost(self):
         return self.shares * self.price
 
+    def sell(self, nshares):
+        self.shares -= nshares
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
